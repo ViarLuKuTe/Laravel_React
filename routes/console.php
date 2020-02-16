6d59862c1f8b98923eb2c8338072eae465e05df5
LuborRod <rodionliuborets@gmail.com>
2020-02-16 23:06:37 +0200

    fourth commit

--- routes/console.php
+++ routes/console.php
@@ -16,4 +16,5 @@ use Illuminate\Foundation\Inspiring;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
     $this->comment(Inspiring::quote());
+    $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
